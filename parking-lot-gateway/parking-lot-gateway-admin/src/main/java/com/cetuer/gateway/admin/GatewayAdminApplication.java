16fc924714de7d8cf4848bdafa1d64d3a3201c3a
BaoBaoss <cetuer@qq.com>
2021-12-06 21:59:43 +0800

    添加网关

---
+++ parking-lot-gateway/parking-lot-gateway-admin/src/main/java/com/cetuer/gateway/admin/GatewayAdminApplication.java
@@ -0,0 +1,16 @@
+package com.cetuer.gateway.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 后台网关启动类
+ *
+ * @author Cetuer
+ * @date 2021/12/5 16:13
+ **/
+@SpringBootApplication
+public class GatewayAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayAdminApplication.class, args);
+    }
+}
